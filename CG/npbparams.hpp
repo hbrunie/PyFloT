62c6db528ee401e13eff9ca26de48803ee4f4185
Hugo Brunie <hbrunie@lbl.gov>
2019-08-28 13:57:24 -0700

    adding CG bench for tests

---
+++ CG/npbparams.hpp
@@ -0,0 +1,21 @@
+/* CLASS = A */
+/*
+c  This file is generated automatically by the setparams utility.
+c  It sets the number of processors and the class_npb of the NPB
+c  in this directory. Do not modify it by hand.
+*/
+#define	NA	14000
+#define	NONZER	11
+#define	NITER	15
+#define	SHIFT	20.0
+#define	RCOND	1.0e-1
+#define	CONVERTDOUBLE	FALSE
+#define COMPILETIME "28 Aug 2019"
+#define NPBVERSION "4.0"
+#define CS1 "g++-mp-9 -std=c++14"
+#define CS2 "$(CC)"
+#define CS3 "-lm "
+#define CS4 "-I../common "
+#define CS5 "-O3"
+#define CS6 "-O3"
+#define CS7 "randdp"
