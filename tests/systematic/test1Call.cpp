cef3b8f38ac832714b7db0a7db2d2ef9953a94b3
Hugo Brunie <hbrunie@lbl.gov>
2019-09-11 19:28:26 -0700

    Adding test for 1 call sites, multiple dynamics calls

--- tests/systematic/test1Call.cpp
+++ tests/systematic/test1Call.cpp
@@ -1,13 +1,26 @@
 #include <cstdlib>
 #include <iostream>
 #include "PT_math.h"
+#define EPSILON 0.0000000001
 using namespace std;
 int main(int argc, char * argv[]){
-    double a = exp(3.14);
+    double a = exp(3.141592);
+    double refA =23.1406775082637;
+    bool success = true;
 #ifdef V1 // Can lower precision 
-    cout << "SUCCESS" << endl;
+    // can lower both
+    cerr << "Can lower precision of all calls " << endl;
 #else
-    cout << "FAILURE" << endl;
+    // can not lower any
+    cerr << "Can not lower any calls " << endl;
+    double err = refA - a;
+    double absErr = err > 0 ? err : -err;
+    if(absErr > EPSILON)
+        success = false;
 #endif
+    if(success)
+        cout << "SUCCESS" << endl;
+    else
+        cout << "FAILURE" << endl;
     return 0;
 }
---
+++ tests/systematic/test1CallSiteMultipleDynCalls.cpp
@@ -0,0 +1,54 @@
+#include <cstdlib>
+#include <iostream>
+#include "PT_math.h"
+#define LOOP 10
+using namespace std;
+int main(int argc, char * argv[]){
+    double a=0.0;
+    for (int i = 0; i<LOOP;i++) {
+        a += exp(3.141592);
+    }
+    double refA =231.406775082637;
+    double err = refA - a;
+    double absErr = err > 0 ? err : -err;
+    cerr << absErr << endl;
+    bool success = true;
+#ifdef V1 // Can lower precision 
+    // can lower both
+    cerr << "Can lower precision of all calls " << endl;
+#elif V2
+    // can not lower any 
+    const double EPSILON = 1E-10;
+    cerr << "Can not lower any calls " << endl;
+    if(absErr > EPSILON)
+        success = false;
+#elif V3
+    // can lower some
+    const double EPSILON=5E-6;
+    cerr << "Can lower some calls? "<< EPSILON << endl;
+    if(absErr > EPSILON)
+        success = false;
+#elif V4
+    // can lower some
+    cerr << "Can lower some calls? "<< 4E-6 << endl;
+    if(absErr > 4E-6)
+        success = false;
+#elif V5
+    // can lower some
+    const double EPSILON=3E-6;
+    cerr << "Can lower some calls? "<< EPSILON << endl;
+    if(absErr > EPSILON)
+        success = false;
+#else
+    // can lower some
+    const double EPSILON=1E-6;
+    cerr << "Can lower some calls? "<< EPSILON << endl;
+    if(absErr > EPSILON)
+        success = false;
+#endif
+    if(success)
+        cout << "SUCCESS" << endl;
+    else
+        cout << "FAILURE" << endl;
+    return 0;
+}
