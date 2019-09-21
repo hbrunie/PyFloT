e8f286ec5efe40e47aeba37f22183b556e43f0b3
Hugo Brunie <hbrunie@lbl.gov>
2019-09-20 18:15:49 -0700

    [TEST] Create specific config and env scripts for MACOS and Cori.

---
+++ tests/trivial/coriScripts/set_env_prof.sh
@@ -0,0 +1,2 @@
+export DYLD_LIBRARY_PATH=/global/cscratch1/sd/hbrunie/tools/precisiontuning//public/lib/:
+export DUMPJSONPROFILINGFILE=/global/cscratch1/sd/hbrunie/tools/precisiontuning/tests/trivial/tmp/profile.json
---
+++ tests/trivial/coriScripts/set_env_strat.sh
@@ -0,0 +1,3 @@
+export DYLD_LIBRARY_PATH=/global/cscratch1/sd/hbrunie/tools/precisiontuning//public/lib/:
+export DUMPJSONSTRATSRESULTSFILE=/global/cscratch1/sd/hbrunie/tools/precisiontuning/tests/trivial/tmp/dumpJsonStratResults_0.json
+export READJSONPROFILESTRATFILE=/global/cscratch1/sd/hbrunie/tools/precisiontuning/tests/trivial/tmp/readJsonStrat_0.json
