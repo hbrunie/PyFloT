12c4306ffaa1ce2515824f959392828733f7ad80
Hugo Brunie <hbrunie@lbl.gov>
2019-09-24 14:18:06 -0700

    [TEST] Cori set env scripts.

---
+++ tests/systematic/coriScripts/set_env_prof.sh
@@ -0,0 +1,2 @@
+export LD_LIBRARY_PATH=/global/cscratch1/sd/hbrunie/tools/precisiontuning/public/lib/:
+export DUMPJSONPROFILINGFILE=/global/cscratch1/sd/hbrunie/tools/precisiontuning/tests/systematic/tmp/profile.json
---
+++ tests/systematic/coriScripts/set_env_strat.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=/global/cscratch1/sd/hbrunie/tools/precisiontuning/public/lib/:
+export DUMPJSONSTRATSRESULTSFILE=/global/cscratch1/sd/hbrunie/tools/precisiontuning/tests/systematic/tmp/dumpJsonStratResults_0.json
+export READJSONPROFILESTRATFILE=/global/cscratch1/sd/hbrunie/tools/precisiontuning/tests/systematic/tmp/readJsonStrat_0.json
