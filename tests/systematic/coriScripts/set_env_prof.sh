c0c93eb2ddbc93854b40da9fa409fa19c7fe2b78
Hugo Brunie <hbrunie@lbl.gov>
2019-09-26 17:52:06 -0700

    [TEST] Adding PRECISION_TUNER_MODE env var.
        PRECISION_TUNER_ in front of env var.
        Follow the commit for C++ and PYTHON.

--- tests/systematic/coriScripts/set_env_prof.sh
+++ tests/systematic/coriScripts/set_env_prof.sh
@@ -1,3 +1,3 @@
 export LD_LIBRARY_PATH=/global/cscratch1/sd/hbrunie/tools/precisiontuning/public/lib/:
-export DUMPJSONPROFILINGFILE=/global/cscratch1/sd/hbrunie/tools/precisiontuning/tests/systematic/tmp/profile.json
+export PRECISION_TUNER_DUMPJSONPROFILINGFILE=/global/cscratch1/sd/hbrunie/tools/precisiontuning/tests/systematic/tmp/profile.json
 export PRECISION_TUNER_MODE=APPLYING_PROF
--- tests/systematic/coriScripts/set_env_strat.sh
+++ tests/systematic/coriScripts/set_env_strat.sh
@@ -1,3 +1,4 @@
 export LD_LIBRARY_PATH=/global/cscratch1/sd/hbrunie/tools/precisiontuning/public/lib/:
-export DUMPJSONSTRATSRESULTSFILE=/global/cscratch1/sd/hbrunie/tools/precisiontuning/tests/systematic/tmp/dumpJsonStratResults_0.json
-export READJSONPROFILESTRATFILE=/global/cscratch1/sd/hbrunie/tools/precisiontuning/tests/systematic/tmp/readJsonStrat_0.json
+export PRECISION_TUNER_DUMPJSONSTRATSRESULTSFILE=/global/cscratch1/sd/hbrunie/tools/precisiontuning/tests/systematic/tmp/dumpJsonStratResults_0.json
+export PRECISION_TUNER_READJSONPROFILESTRATFILE=/global/cscratch1/sd/hbrunie/tools/precisiontuning/tests/systematic/tmp/readJsonStrat_0.json
+export PRECISION_TUNER_MODE=APPLYING_STRAT
