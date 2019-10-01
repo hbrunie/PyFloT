a95966068aa8e129c6db269aa3b1f32955f0c712
Hugo Brunie <hbrunie@lbl.gov>
2019-09-30 18:46:44 -0700

    [PUBLIC] Uncommenting header.

--- public/include/PT_math.h
+++ public/include/PT_math.h
@@ -30,10 +30,10 @@ double __overloaded_fabs(double var, string label);
 double __overloaded_pow(double var, double p, string label);
 
 // FLOAT exponential function
-//#ifdef expf
-//#undef expf
-//#endif
-//#define expf __overloaded_expf
+#ifdef expf
+#undef expf
+#endif
+#define expf __overloaded_expf
 
 // exponential function
 #ifdef exp
@@ -42,44 +42,44 @@ double __overloaded_pow(double var, double p, string label);
 #define exp __overloaded_exp
 
 // logarithm function
-//#ifdef log
-//#undef log
-//#endif
-//#define log __overloaded_log
-//
-//// logarithm function
-//#ifdef log10
-//#undef log10
-//#endif
-//#define log10 __overloaded_log10
-//
-//// cosinus function
-//#ifdef cos
-//#undef cos
-//#endif
-//#define cos __overloaded_cos
-//
-//// sinus function
-//#ifdef sin
-//#undef sin
-//#endif
-//#define sin __overloaded_sin
-//
-//// power function
-//#ifdef pow
-//#undef pow
-//#endif
-//#define pow __overloaded_pow
-//
-//// sqrt function
-//#ifdef sqrt
-//#undef sqrt
-//#endif
-//#define sqrt __overloaded_sqrt
-//
-//// fabs function
-//#ifdef fabs
-//#undef fabs
-//#endif
-//#define fabs __overloaded_fabs
+#ifdef log
+#undef log
+#endif
+#define log __overloaded_log
+
+// logarithm function
+#ifdef log10
+#undef log10
+#endif
+#define log10 __overloaded_log10
+
+// cosinus function
+#ifdef cos
+#undef cos
+#endif
+#define cos __overloaded_cos
+
+// sinus function
+#ifdef sin
+#undef sin
+#endif
+#define sin __overloaded_sin
+
+// power function
+#ifdef pow
+#undef pow
+#endif
+#define pow __overloaded_pow
+
+// sqrt function
+#ifdef sqrt
+#undef sqrt
+#endif
+#define sqrt __overloaded_sqrt
+
+// fabs function
+#ifdef fabs
+#undef fabs
+#endif
+#define fabs __overloaded_fabs
 #endif
