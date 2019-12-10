b48f95a45219fc5fd883cd191ef549aa68de06d8
Hugo Brunie <hbrunie@lbl.gov>
2019-12-09 18:24:12 -0800

    [TESTS] Updating exp test: more dependance, easier to check final result.

--- tests/gotcha/test5Exp.cpp
+++ tests/gotcha/test5Exp.cpp
@@ -1,46 +1,70 @@
 #include <cstdlib>
 #include <iostream>
+#include <iomanip>
 #include "cmath"
 
 #include <PT_Labels.hpp>
 
 #define LOOP 1
+#define EPSILON 0.000000001
 
 using namespace std;
 int main(int ac, char * av[]){
     double a;
+    double refa = atof(av[1]);
     setInRegion("main");
-    for (int i = 0 ; i < LOOP ; i++)
 #ifdef USE_LABEL
-    a = exp(atof(av[1]),"exp1");
+    a = exp(refa,"exp1");
 #else
-    a = exp(atof(av[1]));
+    a = exp(refa);
 #endif
-    for (int i = 0 ; i < LOOP ; i++)
+    a = log(a);
+    for (int i = 0 ; i < LOOP ; i++){
 #ifdef USE_LABEL
-    a = exp(atof(av[1]),"exp1");
+        a = exp(a,"exp1");
 #else
-    a = exp(atof(av[1]));
+        a = exp(a);
 #endif
-    for (int i = 0 ; i < LOOP ; i++)
+        a = log(a);
+    }
+
+    for (int i = 0 ; i < LOOP ; i++){
 #ifdef USE_LABEL
-    a = exp(atof(av[1]),"exp1");
+        a = exp(a,"exp1");
 #else
-    a = exp(atof(av[1]));
+        a = exp(a);
 #endif
-    for (int i = 0 ; i < LOOP ; i++)
+        a = log(a);
+    }
+    for (int i = 0 ; i < LOOP ; i++){
 #ifdef USE_LABEL
-    a = exp(atof(av[1]),"exp1");
+        a = exp(a,"exp1");
 #else
-    a = exp(atof(av[1]));
+        a = exp(a);
 #endif
-    for (int i = 0 ; i < LOOP ; i++)
+        a = log(a);
+    }
+    for (int i = 0 ; i < LOOP ; i++){
 #ifdef USE_LABEL
-    a = exp(atof(av[1]),"exp1");
+        a = exp(a,"exp1");
 #else
-    a = exp(atof(av[1]));
+        a = exp(a);
 #endif
+        a = log(a);
+    }
+    for (int i = 0 ; i < LOOP ; i++){
+#ifdef USE_LABEL
+        a = exp(a,"exp1");
+#else
+        a = exp(a);
+#endif
+        a = log(a);
+    }
     unSetInRegion("main");
-    cout << "FAILURE: "<< a << endl;
+    cout << setprecision(16);
+    if(abs(a - refa) < EPSILON)
+        cout << "SUCCESS: "<< a << " ref: "<< refa << endl;
+    else
+        cout << "FAILURE: "<< a << " ref: "<< refa << endl;
     return 0;
 }
