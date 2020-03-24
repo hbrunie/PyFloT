59f1c1bfafa5a253d429443aad57218c5fddf2ee
Hugo Brunie <hbrunie0@gmail.com>
2020-03-24 09:44:16 -0700

    [PYTHON] Bug subprocess PMF 1D.
    Comment use of subprocess.check_output.
    Seems like stdout or stderr is not closed properly in PMF 1D,
    thus python call deadlock...
    
    Remove lines renaming plt and chk directories after execution.
    add dumpdir creation.

--- public/scripts/Envvars.py
+++ public/scripts/Envvars.py
@@ -1,28 +1,37 @@
 import subprocess
+from subprocess import *
 import os
 
 class Envvars:
     _ENVVAR_DUMPSTRAT                   = "PRECISION_TUNER_DUMPJSONSTRATSRESULTSFILE"
+    _ENVVAR_DUMPDIR                     = "PRECISION_TUNER_OUTPUT_DIRECTORY"
     _ENVVAR_READSTRAT                   = "PRECISION_TUNER_READJSONPROFILESTRATFILE"
     _ENVVAR_PTUNERMODE                  = "PRECISION_TUNER_MODE"
     _ENVVAR_OMPNUMTHREADS               = "OMP_NUM_THREADS"
     _ENVVAR_PTUNERDEBUG                 = "DEBUG"
-    _ENVVAR_PTUNERDUMPPROF ="PRECISION_TUNER_DUMPJSONPROFILINGFILE"
-    _MODE_PROF = "APPLYING_PROF"
+    _ENVVAR_PTUNERDUMPPROF              = "PRECISION_TUNER_DUMPJSON"
+    #_ENVVAR_PTUNERDUMPPROFCSV           = "PRECISION_TUNER_DUMPCSV"
     _MODE_STRAT = "APPLYING_STRAT"
     def __init__(self):
         return None
 
     def execute(self, command, outputfile, procenv):
-        out = ""
-        try:
-            output = subprocess.check_output(
-                    command, stderr=subprocess.STDOUT, shell=True,
-                    universal_newlines=True, env=procenv)
-        except subprocess.CalledProcessError as exc:
-            out = "Status : FAIL\nCode: {}\n{}".format(exc.returncode, exc.output)
-        else:
-            out = "Output: \n{}\n".format(output)
-        with open(outputfile, "w") as ouf:
-            ouf.write(out)
-        return out
+        #out = ""
+        #TODO: subprocess communicate, hang out on maybe
+        # what could be a not properly closed stderr or stdout...
+        # IN PeleC PMF 1D
+        #try:
+        #    output = subprocess.check_output(
+        #            command, stderr=subprocess.STDOUT, shell=True,
+        #            universal_newlines=True, timeout=10, env=procenv)
+        #except subprocess.CalledProcessError as exc:
+        #    out = "Status : FAIL\nCode: {}\n{}".format(exc.returncode, exc.output)
+        #else:
+        #    out = "Output: \n{}\n".format(output)
+        for var,value in procenv.items():
+            os.environ[var] = value
+        print(" ".join(command))
+        os.system(" ".join(command))
+        #with open(outputfile, "w") as ouf:
+        #    ouf.write(out)
+        return ""
--- public/scripts/Profiling.py
+++ public/scripts/Profiling.py
@@ -17,10 +17,12 @@ class Profiling(Envvars):
         self.__onlyGenStrat        = args.onlyGenStrat
         self.__execAllStrat        = args.execAllStrat
         self.__outputFile          = args.outputfile
-        self.__dumpJsonProfileFile = args.ptunerdir + "/" + args.profilefile
+        self.__dumpJsonProfileFile = args.profilefile
         assert self.__dumpJsonProfileFile != "None"
         if not doNotExec:
+            print("Profiling application ...")
             self.executeApplicationProfiling()
+            print("Application profiled")
         return None
 
     def __repr__(self):
@@ -29,23 +31,21 @@ class Profiling(Envvars):
         return s
 
     def execute(self, command, outputfile, procenv):
+        print(command)
         out = super().execute(command,outputfile, procenv)
-        os.system("mv plt00000 plt00000_profile")
-        os.system("mv plt00030 plt00030_profile")
-        os.system("mv chk00000 chk00000_profile")
-        os.system("mv chk00030 chk00030_profile")
         return out
 
     def executeApplicationProfiling(self):
         procenv = os.environ.copy()
         procenv[self._ENVVAR_PTUNERDUMPPROF] = self.__dumpJsonProfileFile
         procenv[self._ENVVAR_OMPNUMTHREADS] = "1"
-        procenv[self._ENVVAR_PTUNERMODE] = self._MODE_PROF
+        procenv[self._ENVVAR_DUMPDIR] = self.__directory
+        os.system("mkdir -p {}".format(self.__directory))
         #procenv["PRECISION_TUNER_DEBUG"] = ""
         command = []
         command.append(self.__binary+" " +self.__param)
         print("PROFILING Command: ",command)
-        outputfile = self.__directory + self.__outputFile+"_profile.txt"
+        outputfile = self.__outputFile+"_profile.txt"
         out = self.execute(command,outputfile,procenv)
 
     def developStrategy(self, stratfiles):
