59f3f200462ab6d650c1bc7b7d61916074eff367
Hugo Brunie <hbrunie0@gmail.com>
2020-04-15 20:18:30 -0700

    [PYTHON] Add static + clustering approach.

---
+++ public/scripts/staticWithClusterApproach.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+from parse import parseStaticWithCluster
+
+from generateStrat import execApplication
+from generateStrat import createStratFilesStatic
+from generateStrat import createStratFilesMultiSiteStatic
+from generateStrat import execApplication
+from generateStrat import execApplicationMultiSite
+from generateStrat import display
+from generateStrat import getVerbose
+from generateStrat import updateProfileCluster
+from generateStrat import getCorrStatList
+
+from communities import build_graph
+from communities import generate_graph
+
+## Parsing arguments
+args           = parseStaticWithCluster()
+params         = args.param
+binary         = args.binary
+dumpdir        = args.dumpdir
+profileFile    = args.profilefile
+checkText2Find = args.verif_text
+tracefile      = args.mergedtracefile
+threshold      = args.threshold
+## Composed constants
+stratDir            = dumpdir + "/strats/static/"
+readJsonProfileFile = dumpdir + "/" + profileFile
+## get verbose level from generateStrat.py
+verbose = getVerbose()
+
+## Generate clusters
+updateProfileCluster(readJsonProfileFile)
+corr = getCorrStatList()
+print(corr)
+(ge, gn) = build_graph(tracefile, corr)
+generate_graph(ge, gn, threshold, corr)
+exit(0)
+
+## Individual analysis (BFS inspired from Mike Lam papers)
+toTestList = createStratFilesStaticCluster(stratDir)
+if verbose >2:
+    print("Level1 Individual: ToTest name list: ", [x[0] for x in toTestList])
+## Get the successful individual static call sites
+validList = execApplication(binary, params, stratDir, toTestList, checkText2Find, dumpdir, profileFile)
+if verbose>2:
+    print("Level1, Valid name list of individual-site static call sites: ", validList[0])
+
+## For all remaining Static Calls
+## Sort all strategies per performance impact,
+## start trying them from the most to the less impact.
+toTestListGen = createStratFilesMultiSiteStatic(stratDir,readJsonProfileFile,validList)
+if verbose>2:
+    print("Level1 Multi-Site ToTest name list: ", [x[0] for x in toTestList])
+
+## Execute the application on generated strategy files
+## Generate strategies choosing k among n.
+## Analyze k-strategies before generating strategies for the next k.
+toStop = True
+while toStop:
+    try:
+        toTestList = next(toTestListGen)
+    except StopIteration:
+        print("No more strategy to test.")
+        display()
+        exit(0)
+    if verbose>2:
+        print("Level1 Multi-Site ToTest name list: ", [x[0] for x in toTestList])
+    validList = execApplicationMultiSite(binary, params, stratDir, toTestList, checkText2Find, dumpdir, profileFile)
+    if verbose>2:
+        if len(validList)>0:
+            print("Level2, Valid Name list of multi-site static call sites:", validList[0])
+    ## valid type configuration found. Stop the search.
+    if len(validList)>0:
+        display()
+        exit(0)
+
+
