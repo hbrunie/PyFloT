a39a2ea52727adb80a2f6aa45b273a2db76656ab
Hugo Brunie <hbrunie0@gmail.com>
2020-04-19 23:11:46 -0700

    [PYTHON] default: runApp mockup but real community algo.

--- public/scripts/Common.py
+++ public/scripts/Common.py
@@ -103,8 +103,8 @@ def clusterBFS(profile, searchSet, params, binary, dumpdir, stratDir, sloc,
     if sloc:
         corr = profile._correspondanceBt2SLOC
     (ge, gn) = build_graph(searchSet, tracefile, threshold, windowSize, corr)
-    #com = community_algorithm(ge, gn, threshold, maxdepth)
-    com = community_algorithm_mockup(gn)
+    com = community_algorithm(ge, gn, threshold, maxdepth)
+    #com = community_algorithm_mockup(gn)
     if not com:
         return (set(), searchSet)
     ## Individual analysis for BFS
@@ -114,8 +114,8 @@ def clusterBFS(profile, searchSet, params, binary, dumpdir, stratDir, sloc,
     ## Get the successful individual sloc/backtrace based call sites
     validDic = {}
     for (name, btCallSiteList) in toTestList:
-        valid = runAppMockup(btCallSiteList, sloc)
         #valid = runApp(cmd, stratDir, name, checkTest2Find, envStr, profile._nbTrials)
+        valid = runAppMockup(btCallSiteList, sloc)
         if valid:
             validDic[name] = btCallSiteList
             profile.trialSuccessIndivCluster(btCallSiteList, sloc)
@@ -180,8 +180,8 @@ def BFS(profile, searchSet, params, binary, dumpdir, stratDir, checkText2Find, v
     ## Get the successful individual static call sites
     validDic = {}
     for (name, CallSiteList) in toTestList:
-        valid = runAppMockup(CallSiteList, sloc)
         #valid = runApp(cmd, stratDir, name, checkText2Find, envStr, profile._nbTrials, btCallSiteList)
+        valid = runAppMockup(CallSiteList, sloc)
         if valid:
             validDic[name] = CallSiteList
             profile.trialSuccessIndivBFS(CallSiteList, sloc)
