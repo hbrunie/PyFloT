d1b2da392d1c0b37ad1c4a003398b704d7638a80
Hugo Brunie <hbrunie0@gmail.com>
2020-05-07 11:06:16 -0700

    [TESTS] Huge refactoring.
    Change directory name.
    Delete obsolete scritps.
    Update config file.

--- tests/trivial/coriScripts/set_env_strat.sh
+++
@@ -1,3 +0,0 @@
-export DYLD_LIBRARY_PATH=/global/cscratch1/sd/hbrunie/tools/precisiontuning//public/lib/:
-export DUMPJSONSTRATSRESULTSFILE=/global/cscratch1/sd/hbrunie/tools/precisiontuning/tests/trivial/tmp/dumpJsonStratResults_0.json
-export READJSONPROFILESTRATFILE=/global/cscratch1/sd/hbrunie/tools/precisiontuning/tests/trivial/tmp/readJsonStrat_0.json
