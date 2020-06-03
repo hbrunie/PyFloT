2c8b02bdb2a6ef4af2ede172a1fe6f2693af96d2
Hugo Brunie <hbrunie0@gmail.com>
2020-06-03 11:22:52 -0700

    [TESTS] tutorial.

--- tests/tutorial/testExp.cpp
+++ tests/tutorial/testExp.cpp
@@ -2,8 +2,68 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-int main(){
-    double a = exp(3.14);
+/* Launch this test with:
+    ${PYFLOT_ROOT}/public/pyflot/profiling.py --binary ./testExp --dumpdir .pyflot-testExp/
+*/
+int main(int ac, char * av[]){
+    double a = exp(atof(av[0]));
     cout << "SUCCESS: "<< a << endl;
     return 0;
 }
+/*
+ * alias pyflot-profiling=${PYFLOT_ROOT}/public/pyflot/profiling.py
+ >>$ pyflot-profiling --binary ./testExp --dumpdir .pyflot-testExp/
+         Profiling application ...
+         Namespace(binary='./testExp', conf_file=None, dumpdir='.pyflot-testExp/', outputfile='output', params='', profilefile='profile.json', verif_text='VERIFICATION SUCCESSFUL')
+         env: TARGET_FILENAME=./testExp PRECISION_TUNER_OUTPUT_DIRECTORY=.pyflot-testExp/ OMP_NUM_THREADS=1 PRECISION_TUNER_DUMPJSON=.pyflot-testExp/profile.json
+         PROFILING Command:  ['./testExp ']
+         ./testExp
+        stargetExe./testExp
+        3
+        targetExe:./testExp
+        ./testExp() [0x400a15]
+        /lib64/libc.so.6(__libc_start_main+0xea) [0x2aaaab85234a]
+        ./testExp() [0x40094a]
+        ./testExp3
+        Application profiled
+
+>>$ ls .pyflot-testExp
+    dumpCSVdynCallSite-0.csv  output                    profile.json
+
+>>$ cat .pyflot-testExp/profile.json
+    {
+      "IndependantCallStacks" :
+      [
+        {
+          "Addr2lineCallStack" :
+          [
+            "/global/u1/h/hbrunie/utils/tools/precisiontuning/tests/tutorial/testExp.cpp:6",
+            "??:0",
+            "/home/abuild/rpmbuild/BUILD/glibc-2.26/csu/../sysdeps/x86_64/start.S:122"
+          ],
+            "CSVFileName" : "dumpCSVdynCallSite-0.csv",
+            "CallStack" :
+          [
+          "./testExp() [0x400a15]",
+          "/lib64/libc.so.6(__libc_start_main+0xea) [0x2aaaab85234a]",
+          "./testExp() [0x40094a]"
+          ],
+          "CallsCount" : 1,
+          "HashKey" : "0x2aaaab85234a",
+          "Index" : 0,
+          "Labels" : null,
+          "LowerBound" : 4294967295,
+          "LowerCount" : 0,
+          "UpperBound" : 0
+        }
+      ],
+      "TotalCallStacks" : 1
+    }
+
+>>$ cat .pyflot-testExp/output
+    SUCCESS: 1
+
+>>$ cat .pyflot-testExp/dumpCSVdynCallSite-0.csv
+    index timeStamp argument doubleP singleP absErr relErr spBoolean callSite
+    0 38.465000 0.000000 1.000000 1.000000 0.000000 0.000000 0 0
+*/
---
+++ tests/tutorial/testNewCallStack2Exp.cpp
@@ -0,0 +1,53 @@
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#define EPSILON 1e-8
+#define FACT1 1./1.
+#define FACT2 1./2./FACT1
+#define FACT3 1./3./FACT2
+#define FACT4 1./4./FACT3
+#define FACT5 1./5./FACT4
+#define FACT6 1./6./FACT5
+#define FACT7 1./7./FACT6
+#define FACT8 1./8./FACT7
+#define FACT9 1./9./FACT8
+#define FACT10 1./10./FACT9
+
+#define TWO seed*seed
+#define THREE seed*TWO
+#define FOUR TWO*TWO
+#define FIVE TWO*THREE
+#define SIX THREE*THREE
+#define SEVEN THREE*FOUR
+#define HEIGHT FOUR*FOUR
+#define NINE FIVE*FOUR
+#define TEN FIVE*FIVE
+using namespace std;
+int main(int ac, char * av[]){
+    srand((unsigned int )NULL);
+    double seed = (double)(rand())/(double)RAND_MAX;
+    double expApprox = 1+seed + TWO/FACT2 + THREE/FACT3 + FOUR/FACT4 + FIVE/FACT5 + SIX/FACT6 + SEVEN/FACT7 + HEIGHT/FACT8 + NINE/FACT9 + TEN/FACT10;
+    //float printedResult = exp((float)seed);
+    //double printedResult = exp((float)seed);
+    double printedResult = exp(seed);
+    double anotherExp = exp(seed*seed);
+    cerr << setprecision(16) ;
+    cerr <<"seed: "<< seed << endl;
+    cerr <<"expApprox: "<< expApprox << endl;
+    cerr <<"printedResult: "<< printedResult << endl;
+    //if result not correct enough, take a new path
+    double diff = abs(expApprox-printedResult) ;
+    double relDiff = diff/expApprox ;
+    cerr << "Diff: " << diff <<endl;
+    cerr << "RelDiff: " << relDiff <<endl;
+    cerr << "epsilon: " << EPSILON <<endl;
+    if(relDiff < EPSILON)
+        cout << "SUCCESS" << endl;
+    else{
+        printedResult += exp(seed+seed);
+        cout << "FAILURE: "<< printedResult << endl;
+    }
+    cout << "another: " << anotherExp << endl;
+    return 0;
+}
