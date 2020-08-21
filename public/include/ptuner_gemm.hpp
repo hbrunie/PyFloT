2b64acf49718d43df62177cdd06dbe9520e057b6
Hugo Brunie <hbrunie0@gmail.com>
2020-08-21 10:04:52 -0700

    [PUBLIC] Header for ptuner gemm.

---
+++ public/include/ptuner_gemm.hpp
@@ -0,0 +1,3 @@
+void ptuner_dgemm(char const *transa, char const *transb, int *m, int *n, int *k,
+              double *alpha, double *A, int *lda, double *B, int *ldb,
+              double *beta, double *C, int *ldc);
