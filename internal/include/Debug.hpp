56b1c593b9ff1537931f7eced803086f21a8fed7
Hugo Brunie <hbrunie0@gmail.com>
2020-08-21 10:05:28 -0700

    [C++] Move Debug utils files.

--- internal/include/Debug.hpp
+++ internal/include/Debug.hpp
@@ -1,26 +1,30 @@
 #ifndef DEBUG_H
 #define DEBUG_H
+#include <iostream>
+#include <vector>
 
 #ifndef NDEBUG
-
-#include <iostream>
-  extern bool DebugFlag;
-
-  void debugtypeOption(char *env);
-  bool isCurrentDebugType(const char *type);
-
-  void setCurrentDebugType(const char *type);
-
 #define DEBUG(TYPE, X)					\
-  do {if (DebugFlag && isCurrentDebugType(TYPE)) { X; }	\
+  do {if (debug.DebugFlag && debug.isCurrentDebugType(TYPE)) { X; }	\
   } while (0)
-
 #define DEBUGG(TYPE, X) DEBUG(TYPE,std::cerr << __FUNCTION__<< ": " << X << endl;);
 #define DEBUGINFO(X) DEBUGG("info",X);
 
+class Debug
+{
+
+    private:
+        std::vector<std::string> CurrentDebugType;
+        void setCurrentDebugType(const char *type);
+    public:
+        Debug();
+        void debugtypeOption(char *env);
+        bool DebugFlag = false;
+        bool isCurrentDebugType(const char *type);
+};
+
 #else
-//#define isCurrentDebugType(X) (false)
-//#define setCurrentDebugType(X)
+
 #define DEBUG(TYPE, X) do { } while (0)
 #define DEBUGG(TYPE, X) do { } while (0)
 #define DEBUGINFO(X) do { } while (0)
