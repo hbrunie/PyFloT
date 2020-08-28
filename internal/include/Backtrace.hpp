84c348e401fed87fae4c8e249491aa1ccaae86ce
Hugo Brunie <hbrunie0@gmail.com>
2020-08-27 21:29:44 -0700

    Fix link error(header diff from source).

--- internal/include/Backtrace.hpp
+++ internal/include/Backtrace.hpp
@@ -2,5 +2,6 @@
 #include <vector>
 #define BACKTRACE_MAX 1024
 
-std::vector<std::string> addr2lineBacktraceVec(std::string targetExecutable, std::vector<std::string> bt_syms, size_t bt_size);
+std::vector<std::string> addr2lineBacktraceVec(std::string targetExecutable,
+        std::vector<std::string> &bt_syms, size_t bt_size);
 std::string addr2lineBacktrace(char * targetExecutable, std::string bt_sym);
